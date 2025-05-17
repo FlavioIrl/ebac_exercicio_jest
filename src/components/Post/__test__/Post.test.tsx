29a609efda030be849d0ee021752c21518559b20
Flávio Irala <flavioiralap@gmail.com>
2025-05-16 20:42:09 -0400

    Test para Post concluido!

--- src/components/Post/__test__/Post.test.tsx
+++ src/components/Post/__test__/Post.test.tsx
@@ -1,1 +1,24 @@
 import Post from ".."
+import { screen, render } from "@testing-library/react"
+
+jest.mock('../PostComments', () => () => <div data-testid="post-comments">Comentários</div>)
+
+const mocks = {
+ ImagemMock: "https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg",
+ childrenMock: 'Novo post' 
+}
+
+describe('Testes para o post', () => {
+    test('Deve rendenizar corretamente o post', () => {
+        render(<Post imageUrl={mocks.ImagemMock}>{mocks.childrenMock}</Post>)
+    
+        const imagem = screen.getByTestId('ImagemUrl') 
+        const conteudo = screen.getByTestId('ContPost') 
+
+        expect(imagem).toBeInTheDocument()
+        expect(imagem).toHaveAttribute('src', mocks.ImagemMock)
+        expect(conteudo).toBeInTheDocument()
+        expect(conteudo).toHaveTextContent('Novo post')
+    }) 
+})
+ 
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -10,8 +10,8 @@ type Props = {
 
 const Post = ({ children, imageUrl }: Props) => (
     <div className={styles.post}>
-        <img className={styles['post-image']} src={imageUrl} />
-        <p className={styles['post-text']}> {children} </p>
+        <img data-testid='ImagemUrl' className={styles['post-image']} src={imageUrl} />
+        <p data-testid='ContPost' className={styles['post-text']}> {children} </p>
         <PostComments />
     </div>
 );
